94c806792dfd773721e44d7cc35b75d2e0fcddc6
Iñaki García Blasco <inaki@txorua.com>
2018-02-07 18:50:42 +0100

    Cambio gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,45 +6,10 @@ const imageResize = require('gulp-image-resize')
 const del = require('del')
 const newer = require('gulp-newer')
 
-gulp.task('resize-images-all', () => {
-    const front_end_images = gulp.src('assets/images/uploads/*')
-
-    front_end_images
-        .pipe(imageResize({
-            width: 1300,
-            height: 975,
-            crop: true,
-            upscale: false,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('assets/images/large'))
-
-    front_end_images
-        .pipe(imageResize({
-            width: 650,
-            height: 487,
-            crop: true,
-            upscale: false,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('assets/images/medium'))
-
-    front_end_images
-        .pipe(imageResize({
-            width: 300,
-            height: 225,
-            crop: true,
-            upscale: false,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('assets/images/small'))
-})
-
 gulp.task('resize-images', () => {
     const front_end_images = gulp.src('assets/images/uploads/*')
 
     front_end_images
-        .pipe(newer('assets/images/large'))
         .pipe(imageResize({
             width: 1300,
             height: 975,
@@ -55,7 +20,6 @@ gulp.task('resize-images', () => {
         .pipe(gulp.dest('assets/images/large'))
 
     front_end_images
-        .pipe(newer('assets/images/medium'))
         .pipe(imageResize({
             width: 650,
             height: 487,
@@ -66,7 +30,6 @@ gulp.task('resize-images', () => {
         .pipe(gulp.dest('assets/images/medium'))
 
     front_end_images
-        .pipe(newer('assets/images/small'))
         .pipe(imageResize({
             width: 300,
             height: 225,
@@ -85,4 +48,4 @@ gulp.task('jekyll-build', function (done) {
   return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'}).on('close', done)
 })
 
-gulp.task('default', ['resize-images-all','jekyll-clean','jekyll-build'])
+gulp.task('default', ['resize-images','jekyll-build'])
